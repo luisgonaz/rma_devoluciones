9c1938a2b6355d964d0ce732a7285ab10158c8a4
luis <angelchicles97@gmail.com>
2019-02-18 13:23:07 -0600

    modificacion al modulo rma_ept

---
+++ rma_devo/models/models.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+from odoo.exceptions import UserError
+
+
+class ValidationRma_n(models.Model):
+
+	_inherit = 'stock.picking'
+
+
+class ValidationR(models.Model):
+
+	_inherit ='crm.claim.ept'
+
+
+	purchase_id = fields.Many2one('purchase.order', string="Purcharse_id", compute="onchange_picking_id_add")
+
+
+	state = fields.Selection([('draft','Draft'),('approve','Approved'),('review','Review piece'),('state','good condition'),('default','manufacturing'),('process','Processing'),('close','Closed'),('reject','Rejected')])
+
+
+
+	@api.onchange('picking_id')
+	def onchange_picking_id_add(self):
+		if self.picking_id:
+			valor = self.picking_id.purchase_id.id
+			self.purchase_id = valor
+	
+
+
+
+
+
+		
+
+
+
+
+
